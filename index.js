fa153ac2cc553d64219d43b6d681e39acf5734ab
SaadTK <tahmid.karim.saad@gmail.com>
2025-06-09 22:08:11 +0600

    after deployment 2

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin:
-      "https://insta-nest-client-hk6b6md16-tahmid-karim-saads-projects.vercel.app/",
+      "https://insta-nest-client-hk6b6md16-tahmid-karim-saads-projects.vercel.app",
     credentials: true,
   })
 );
