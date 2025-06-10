a21d0bd70949a8f51ef3b389b1bf1d99e3400f55
SaadTK <tahmid.karim.saad@gmail.com>
2025-06-10 08:19:35 +0600

    after deplying 4

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -42,7 +42,7 @@ export const loginUser = async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "Strict",
+        sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       })
       .status(200)
